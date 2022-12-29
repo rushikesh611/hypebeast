dee3bf37e351cef6a84047525971093e138b6ec2
Hrushikesh Jadhav <rushi611@gmail.com>
2022-12-29 20:47:41 +0530

    apollo setup nextjs

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,22 @@
+import { gql } from "@apollo/client/core";
+import { useQuery } from "@apollo/client/react";
+
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
   return (
     <div>
-      <h1 className="text-lg">hypebeast</h1>
+      <h1 className="text-lg">{JSON.stringify(data)}</h1>
     </div>
   );
 };
